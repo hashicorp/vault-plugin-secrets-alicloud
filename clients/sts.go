0aefb4f5fce8f2b115dbeda8d4c84be90bdb21d9
ethan256 <yuanliang.zhang120@gmail.com>
2022-11-06 17:59:02 +0800

    use sts vpc endpoint

--- clients/sts.go
+++ clients/sts.go
@@ -1,8 +1,12 @@
 package clients
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/sts"
+	"github.com/go-ping/ping"
 )
 
 func NewSTSClient(sdkConfig *sdk.Config, key, secret string) (*STSClient, error) {
@@ -12,11 +16,28 @@ func NewSTSClient(sdkConfig *sdk.Config, key, secret string) (*STSClient, error)
 	}
 	// We hard-code a region here because there's only one RAM endpoint regardless of the
 	// region you're in.
-	client, err := sts.NewClientWithOptions("us-east-1", sdkConfig, creds)
+	regionId := os.Getenv("STS_REGION_ID")
+	if len(regionId) == 0 {
+		regionId = "us-east-1"
+	}
+	client, err := sts.NewClientWithOptions(regionId, sdkConfig, creds)
 	if err != nil {
 		return nil, err
 	}
+	vpcEndpoint := fmt.Sprintf("sts-vpc.%s.aliyuncs.com", regionId)
+	if pingVpcEndpoint(vpcEndpoint) {
+		client.Domain = vpcEndpoint
+	}
 	return &STSClient{client: client}, nil
+}
+
+func pingVpcEndpoint(endpoint string) bool {
+	pinger, err := ping.NewPinger(endpoint)
+	if err != nil {
+		return false
+	}
+	pinger.Count = 1
+	return pinger.Run() == nil
 }
 
 type STSClient struct {
--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/hashicorp/vault-plugin-secrets-alicloud
 go 1.12
 
 require (
-	github.com/aliyun/alibaba-cloud-sdk-go v0.0.0-20190620160927-9418d7b0cd0f
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1843
+	github.com/go-ping/ping v1.1.0
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/go-hclog v1.0.0
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/hashicorp/go-uuid v1.0.2
 	github.com/hashicorp/vault/api v1.3.0
 	github.com/hashicorp/vault/sdk v0.3.0
 	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
-	github.com/smartystreets/goconvey v1.6.4 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 )
