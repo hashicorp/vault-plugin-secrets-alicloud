724f35abe3506ff97473dab522992a3be89b2cd7
Austin Gebauer <34121980+austingebauer@users.noreply.github.com>
2023-02-09 14:39:48 -0800

    Adds changelog file and formats code (#67)
    * Adds changelog and formats code
    
    * upgrade vault SDK and API modules

--- clients/creds.go
+++ clients/creds.go
@@ -10,14 +10,13 @@ import (
 
 // chainedCreds allows us to grab AliCloud credentials in the following order:
 //
-// 		1. Credentials set via env variables if available
-// 		2. Credentials explicitly configured if available
-// 		3. Credentials inferred from instance metadata if available
+//  1. Credentials set via env variables if available
+//  2. Credentials explicitly configured if available
+//  3. Credentials inferred from instance metadata if available
 //
 // Since credentials are pulled every time a client is created, and clients are not
 // long-lasting, this means that credentials could be changed or overridden at any
 // point and they would be picked up without requiring a Vault restart.
-//
 func chainedCreds(key, secret string) (auth.Credential, error) {
 	providerChain := []providers.Provider{
 		providers.NewEnvCredentialProvider(),
--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/hashicorp/go-hclog v1.0.0
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/hashicorp/go-uuid v1.0.2
-	github.com/hashicorp/vault/api v1.3.0
-	github.com/hashicorp/vault/sdk v0.3.0
+	github.com/hashicorp/vault/api v1.8.3
+	github.com/hashicorp/vault/sdk v0.7.0
 	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
